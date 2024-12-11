49baa0ee910fbd10601f78742928385370a5c343
Giacomo Baldessari <103346031+gbaldessari@users.noreply.github.com>
2024-12-11 04:02:15 -0300

    Se hizo lo que se pudo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,104 @@
-import { Body, Controller, Post, BadRequestException, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { lastValueFrom } from 'rxjs';
-
-type RegisterUserDto = {
-  Name: string;
-  LastName: string;
-  Email: string;
-  Password: string;
-}
+import { Body, Controller, Post } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { LoginResponse, ServiceResponse, UserForgotPassword, UserLogin, UserRefreshToken, UserRegister, UserResetPassword } from './users.types';
 
 @Controller('users')
 export class UsersController {
-  constructor(@Inject('USERS_SERVICE') private readonly client: ClientProxy) {}
+  private readonly microserviceUrl = this.configService.get<string>('MICROSERVICE_URL');
+
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) { }
 
   @Post('register')
-  async register(@Body() userData: RegisterUserDto) {
+  async register(@Body() payload: UserRegister) {
     try {
-      return await lastValueFrom(this.client.send({ cmd: 'register_user' }, userData));
+      const axiosResponse = await this.httpService.post(`${this.microserviceUrl}/register`, payload).toPromise();
+      const response: ServiceResponse<string> = {
+        data: axiosResponse?.data.data,
+        success: true,
+      };
+      return response;
     } catch (error) {
-      throw new BadRequestException('Failed to register user.');
+      const response: ServiceResponse<string> = {
+        error: (error as any).message,
+        success: false,
+      };
+      return response;
+    }
+  }
+
+  @Post('login')
+  async login(@Body() payload: UserLogin) {
+    try {
+      const axiosResponse = await this.httpService.post(`${this.microserviceUrl}/login`, payload).toPromise();
+      const response: ServiceResponse<LoginResponse> = {
+        data: axiosResponse?.data.data,
+        success: true,
+      };
+      return response;
+    } catch (error) {
+      const response: ServiceResponse<LoginResponse> = {
+        error: (error as any).message,
+        success: false,
+      };
+      return response;
+    }
+  }
+
+  @Post('refresh-token')
+  async refreshToken(@Body() payload: UserRefreshToken) {
+    try {
+      const axiosResponse = await this.httpService.post(`${this.microserviceUrl}/refresh-token`, payload).toPromise();
+      const response: ServiceResponse<LoginResponse> = {
+        data: axiosResponse?.data.data,
+        success: true,
+      };
+      return response;
+    } catch (error) {
+      const response: ServiceResponse<LoginResponse> = {
+        error: (error as any).message,
+        success: false,
+      };
+      return response;
+    }
+  }
+
+  @Post('forgot-password')
+  async forgotPassword(@Body() payload: UserForgotPassword) {
+    try {
+      const axiosResponse = await this.httpService.post(`${this.microserviceUrl}/forgot-password`, payload).toPromise();
+      const response: ServiceResponse<string> = {
+        data: axiosResponse?.data.data,
+        success: true,
+      };
+      return response;
+    } catch (error) {
+      const response: ServiceResponse<string> = {
+        error: (error as any).message,
+        success: false,
+      };
+      return response;
+    }
+  }
+
+  @Post('reset-password')
+  async resetPassword(@Body() payload: UserResetPassword) {
+    try {
+      const axiosResponse = await this.httpService.post(`${this.microserviceUrl}/reset-password`, payload).toPromise();
+      const response: ServiceResponse<string> = {
+        data: axiosResponse?.data.data,
+        success: true,
+      };
+      return response;
+    } catch (error) {
+      const response: ServiceResponse<string> = {
+        error: (error as any).message,
+        success: false,
+      };
+      return response;
     }
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,30 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { HttpModule } from '@nestjs/axios';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersController } from './users.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ClientsModule.registerAsync([
-      {
-        name: 'USERS_SERVICE',
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [configService.get<string>('RABBITMQ_URL')].filter(url => url !== undefined) as string[],
-            queue: configService.get<string>('RABBITMQ_QUEUE'),
-            queueOptions: {
-              durable: true,
-            },
-          },
-        }),
-      },
-    ]),
+    HttpModule,
   ],
   controllers: [UsersController],
-  providers: [],
+  providers: [ConfigService],
 })
 export class UsersModule {}
